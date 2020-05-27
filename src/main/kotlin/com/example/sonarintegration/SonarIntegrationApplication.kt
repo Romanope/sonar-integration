f9cd69d3150a8d30ee1446fa3536f08581957f01
s2it_cpereira <s2it_cpereira@uolinc.com>
2020-05-27 20:33:12 -0300

    fisrt commit

---
+++ src/main/kotlin/com/example/sonarintegration/SonarIntegrationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sonarintegration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SonarIntegrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SonarIntegrationApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/sonarintegration/SonarIntegrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sonarintegration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SonarIntegrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
